afb9d0c877e7c73dfc13be58b34d3d8914f8e1f7
Shuming Zhao <shuming.zhao@scsstudent.org>
2022-12-10 14:49:37 -0800

    Prime Number Tester

---
+++ src/TestPrime.java
@@ -0,0 +1,35 @@
+import java.lang.Math;
+
+public class TestPrime {
+	private int test;
+	private int k = 1;
+	
+	public TestPrime(int input)
+	{
+		test = input;
+	}
+	
+	public boolean isPrime(int test)
+	{
+		for (int i = 2; i < (int)(Math.sqrt(test)); i++)
+		{
+			if (test % i == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public int findDivisor(int test)
+	{
+		for (int i = 2; i < (int)(Math.sqrt(test)); i++)
+		{
+			if (test % i == 0)
+			{
+				return i;
+			}
+		}
+		return k;
+	}
+}
