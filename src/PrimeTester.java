45e74303731ddde77c3b5ea14e8f13bf4abf91fc
Shuming Zhao <shuming.zhao@scsstudent.org>
2022-12-10 20:48:06 -0800

    Added Prime factorization, Program Self-Iteration

--- src/PrimeTester.java
+++ src/PrimeTester.java
@@ -2,16 +2,33 @@ import java.util.Scanner;
 
 public class PrimeTester {
 	public static void main(String[] args) {
-		System.out.println("Enter an integer to test if it's a prime number: "); 
-	    Scanner scanner = new Scanner(System.in);
-	    int num = scanner.nextInt();
-	    TestPrime machine = new TestPrime(num);
-	    boolean y = machine.isPrime(num);
-	    System.out.println("Is " + num + " prime?");
-	    System.out.println(y);
-	    if (y == false)
-	    {
-	    	System.out.println(num + " can be divided by " + machine.findDivisor(num));
-	    }
+		boolean run = true;
+		while (run == true)
+		{
+			System.out.println("Enter an integer to test if it's a prime number: (Enter 0 to exit)"); 
+	    	Scanner scanner = new Scanner(System.in);
+	    	int num = scanner.nextInt();
+			int origin = num;
+			if (num == 0)
+			{
+				run = false;
+			}
+			else 
+			{
+				TestPrime machine = new TestPrime(num);
+				boolean y = machine.isPrime(num);
+				System.out.println("Is " + num + " prime?");
+				System.out.println(y);
+				while (y == false)
+				{
+					System.out.println(num + " divided by " + machine.findDivisor(num) + " is " + num / machine.findDivisor(num));
+					num = num / machine.findDivisor(num);
+					y = machine.isPrime(num);
+				}
+				System.out.println(num + " is the largest prime factor of " + origin);
+			}
+	    	
+		}
+		
 	}
 }
